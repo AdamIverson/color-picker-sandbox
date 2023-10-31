2f28f1be5c13db5bab045aa250ad6ee2b3aefb5d
Adam Iverson <aciverso@gmail.com>
2023-10-31 08:23:19 -0500

    react-colorful picker built with swatches, text, picker, display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { PopoverPicker } from "./PopoverPicker";
+import "./App.css";
+import { HexColorInput } from "react-colorful";
 
-function App() {
+const App = () => {
+  const [primary, setPrimary] = useState("#FF0000");
+  const [secondary, setSecondary] = useState("#0000FF");
+  const [tertiary, setTertiary] = useState("#008000");
+
+  const presetColors = [primary, secondary, tertiary];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        textAlign: "center",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <div>
+        <label htmlFor={primary} style={{ display: "block" }}>
+          Primary
+        </label>
+        <PopoverPicker
+          color={primary}
+          onChange={setPrimary}
+          presetColors={presetColors}
+        />
+      </div>
+      <div>
+        <label htmlFor={primary} style={{ display: "block" }}>
+          Secondary
+        </label>
+        <PopoverPicker
+          color={secondary}
+          onChange={setSecondary}
+          presetColors={presetColors}
+        />
+      </div>
+      <div style={{ display: "inline" }}>
+        <label htmlFor="tertiary" style={{ display: "block" }}>
+          Tertiary
+        </label>
+        <PopoverPicker
+          color={tertiary}
+          onChange={setTertiary}
+          presetColors={presetColors}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/PopoverPicker.jsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useRef, useState } from "react";
+import { HexColorPicker, HexColorInput } from "react-colorful";
+import "./App.css";
+import { useDebounce } from "use-debounce";
+import useClickOutside from "./useClickOutside";
+
+export const PopoverPicker = ({ color, onChange, presetColors }) => {
+  const [value, setValue] = useState(color);
+  const popover = useRef();
+  const [isOpen, toggle] = useState(false);
+
+  const close = useCallback(() => toggle(false), []);
+  useClickOutside(popover, close);
+  useDebounce(() => onChange(value), 200, [value]);
+
+  return (
+    <div className="picker" style={{ margin: "50px" }}>
+      <div
+        style={{ alignItems: "center", backgroundColor: color }}
+        className="swatch"
+        onClick={() => toggle(true)}
+      />
+      <div>
+        <HexColorInput alpha prefixed color={value} onChange={onChange} />
+      </div>
+      <div className="picker__swatches">
+        {presetColors.map((presetColor) => (
+          <button
+            key={presetColor}
+            className="picker__swatch"
+            style={{ background: presetColor }}
+            onClick={() => onChange(presetColor)}
+          />
+        ))}
+      </div>
+      <div style={{ textAlign: "center" }}>{color}</div>
+
+      {isOpen && (
+        <div className="popover" ref={popover}>
+          <HexColorPicker color={color} onChange={onChange} />
+        </div>
+      )}
+    </div>
+  );
+};
